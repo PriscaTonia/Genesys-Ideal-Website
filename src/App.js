5d1e626bd1b358b8dd45f705809d4a0a24ca1687
PriscaTonia <priscaebube@gmail.com>
2020-10-12 23:08:06 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-     <h1>Hello Everyone</h1>
+     <h1>Hello Everyone, I'm testing this out.</h1>
     </div>
   );
 }
